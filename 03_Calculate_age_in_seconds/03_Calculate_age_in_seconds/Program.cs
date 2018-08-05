ff77ecd61c91d7b0dbabdc288762a86d066f6beb
Adam Błaszczyk <210142@edu.p.lodz.pl>
2018-08-05 18:51:17 +0200

    Zakonczenie projektu 03 v001

---
+++ 03_Calculate_age_in_seconds/03_Calculate_age_in_seconds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Calculate_age_in_seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year, month, day;
+            Console.Write("Year: ");
+            year = int.Parse(Console.ReadLine());
+            Console.Write("Month: ");
+            month = int.Parse(Console.ReadLine());
+            Console.Write("Day: ");
+            day = int.Parse(Console.ReadLine());
+
+            DateTime now = DateTime.Now;          
+            DateTime date = new DateTime(year, month, day);
+
+            Console.WriteLine("Difference in seconds: " + Math.Round((now - date).TotalSeconds));
+            Console.ReadKey();
+        }
+    }
+}
