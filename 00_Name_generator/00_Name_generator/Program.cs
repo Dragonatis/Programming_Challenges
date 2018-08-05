38edaa95f0020f92507716b529856ca34b8a5faa
Adam Błaszczyk <210142@edu.p.lodz.pl>
2018-08-05 17:38:09 +0200

    Zakończenie projektu 00 Wersja projektu 001

--- 00_Name_generator/00_Name_generator/Program.cs
+++ 00_Name_generator/00_Name_generator/Program.cs
@@ -10,7 +10,26 @@ namespace _00_Name_generator
     {
         static void Main(string[] args)
         {
-            // Zmiana
+            Random random = new Random(); 
+            string[] syllables = { "en", "le", "lo", "don", "te", "per", "lni", "kan", "mit",
+                "man", "ka", "sa", "ki", "shi", "mo", "hi", "yo", "wa", "ri", "ise", "ni", "bo", "me", "zu" };
+
+            while(true)
+            {
+                string name = "";
+                int lenght = random.Next(3, 8);
+                
+                while (name.Length < lenght)
+                {
+                    int index = random.Next(syllables.Length);
+                    name += syllables[index];
+                }
+                name = name.First().ToString().ToUpper() + name.Substring(1);
+
+                Console.WriteLine(name);
+                Console.ReadKey();
+            }
+
         }
     }
 }
